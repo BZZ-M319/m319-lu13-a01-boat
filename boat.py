cf2f7587dc249b5e52389f631707a8fd04a6be96
Marcel Suter <marcel.suter@bzz.ch>
2024-01-10 09:03:33 +0100

    removed unused workflow minor changes to codingstyle

--- boat.py
+++ boat.py
@@ -3,6 +3,9 @@ from dataclasses import dataclass
 
 @dataclass
 class Boat:
+    """
+    Dataclass for a boat
+    """
     make: str
     model: str
     year: int
@@ -11,10 +14,13 @@ class Boat:
 
     def print_infos(self):
         print(
-            f"Make: {self.make}, Model: {self.model}, Year: {self.year}, Length: {self.length}, Serial Number: {self.serial_number}")
+            f'Make: {self.make}, Model: {self.model}, Year: {self.year}, Length: {self.length}, '
+            f'Serial Number: {self.serial_number}'
+        )
 
     def __str__(self):
-        return f"Make: {self.make}, Model: {self.model}, Year: {self.year}, Length: {self.length}, Serial Number: {self.serial_number}"
+        return f'Make: {self.make}, Model: {self.model}, Year: {self.year}, Length: {self.length}, ' \
+               f'Serial Number: {self.serial_number}'
 
     def __lt__(self, other):
         if self.length == other.length:
